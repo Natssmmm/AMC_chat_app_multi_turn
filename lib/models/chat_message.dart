ebcc979e15b6c2016851d73613d1c456a1434798
Natss <168500422+Natssmmm@users.noreply.github.com>
2026-02-03 02:34:21 +0800

    Update ChatMessage class structure
    Refactored ChatMessage class to include role instead of isUserMessage.

--- lib/models/chat_message.dart
+++ lib/models/chat_message.dart
@@ -1,18 +1,4 @@
-/*
-class ChatMessage {
-  final String text;
-  final bool isUserMessage;
-  final DateTime timestamp;
 
-
-  ChatMessage({
-
-    required this.text,
-    required this.isUserMessage,
-    required this.timestamp,
-});
-}
-*/
 
 
 class ChatMessage {
